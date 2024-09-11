8a6ec84cea41f74ab3917b8448693b60d0c49eda
Amanda Calado <111512424+amandacaladoac@users.noreply.github.com>
2024-09-11 13:50:32 +0000

    att aula

--- amanda/aula.js
+++ amanda/aula.js
@@ -16,3 +16,32 @@ const numero = 10
 // const numero = 20
 // numero = 30
 console.log(numero) 
+
+// TIPOS DE DADOS 
+// STRING - "Hello World" - 'Hello World' - `Hello World`
+   console.log('Hello' + ' ' + 'World')
+   console.log('Hello' + 'World')
+
+   myName = "Maria"
+   console.log("Hello" + ' ' + myName)
+
+   myName2 = "2"
+   console.log(`Hello ${myName2}`)
+
+// TIPOS PRIMITIVOS 
+// BOLEAN - true/false
+// Undefined - Valor não definido
+// NULL - Representa um valor vazio
+// Number - Valores numéricos, podem ser inteiros ou decimais ('1', '1.1')
+
+// OPERADORES
+// lógicos, comparação e matemáticos
+
+// dois iguais ele compara a imagem
+console.log(2 == "2")
+
+// três iguais ele compara o tipo de dado
+console.log(2 === "2")
+
+
+console.log(15 > 10)
